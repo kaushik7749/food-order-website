400e0ab999cc84aa270efc3d935d431b89721a31
Kaushik Yadure <kaushikyadure4977@gmail.com>
2022-03-23 13:17:16 +0530

    first commit

---
+++ src/components/common/header/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./header.css";
+
+
+const Header = () => {
+  return (
+    <div className="header max-width">
+      <img
+        src="https://foodhub.co.uk/compressed_images/logo.svg"
+        alt="-food-logo"
+        className="header-logo"
+      />
+      <div className="header-right">
+        <div className="header-location-search-container">
+          <div className="location-wrapper">
+            <div className="location-icon-name">
+            <i className="fi fi-rr-marker"></i>
+              <div>Bangalore</div>
+            </div>
+            <i className="fi fi-rr-caret-down absolute-center"></i>
+          </div>
+          <div className="location-search-separator"></div>
+          <div className="header-searchBar">
+            <i className="fi fi-rr-search absolute-center search-icon"></i>
+            <input
+              className="search-input"
+              placeholder="Search for restaurant, cuisine or a dish"
+            />
+          </div>
+        </div>
+
+        <div className="profile-wrapper">
+          <img
+            src="https://b.zmtcdn.com/images/user_avatars/mug_2x.png?fit=around%7C100%3A100&crop=100%3A100%3B%2A%2C%2A"
+            className="header-profile-image"
+            alt="Profile"
+          />
+          <span className="header-username">Kaushik</span>
+          <i className="fi fi-rr-angle-small-down absolute-center profile-options-icon"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
